1cbd4a6c2f3924ea9ee4d73fbaf7efb3d2cb93b1
Naman Agrawal <naman2003now@gmail.com>
2024-04-18 22:29:38 +0530

    bugfix: flake install

--- default.nix
+++ default.nix
@@ -1,10 +1,7 @@
-(import
-  (
-    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
-    fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-      sha256 = lock.nodes.flake-compat.locked.narHash;
-    }
-  )
-  { src = ./.; }
-).defaultNix
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.rustPlatform.buildRustPackage rec {
+  pname = "myecho";
+  version = "0.1";
+  cargoLock.lockFile = ./Cargo.lock;
+  src = pkgs.lib.cleanSource ./.;
+}
--- flake.nix
+++ flake.nix
@@ -1,46 +1,19 @@
 {
+  description = "My clone of echo written in rust.";
   inputs = {
-    naersk.url = "github:nmattia/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = github:edolstra/flake-compat;
-      flake = false;
-    };
   };
-
-  outputs = { self, nixpkgs, utils, naersk, ... }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage {
-          src = ./.;
-          doCheck = true;
-          pname = "myecho";
-        };
-        nativeBuildInputs = [ pkgs.makeWrapper ];
-        installPhase = ''
-          cp target/release/myecho $out/bin/
-        '';
-        postInstall = ''
-          wrapProgram "$out/bin/myecho""
-        '';
-        defaultApp = utils.lib.mkApp {
-          drv = self.defaultPackage."${system}";
-        };
-
-        devShell = with pkgs; mkShell {
-          buildInputs = [
-            cargo
-            rust-analyzer
-            rustPackages.clippy
-            rustc
-            rustfmt
-          ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = nixpkgs.legacyPackages;
+    in {
+      packages = forAllSystems (system: {
+        default = pkgsFor.${system}.callPackage ./default.nix { };
       });
+      devShells = forAllSystems (system: {
+        default = pkgsFor.${system}.callPackage ./shell.nix { };
+      });
+    };
 }
